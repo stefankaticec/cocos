fa99152587c503601d09f40aa262be48339e7109
Stefan Katic <skatic@execom.eu>
2020-12-01 12:45:50 +0100

    Fix dropped connection. Add more information to logs.

--- hub/src/main/java/to/etc/cocos/hub/parties/Cluster.java
+++ hub/src/main/java/to/etc/cocos/hub/parties/Cluster.java
@@ -52,11 +52,14 @@ final public class Cluster {
 		return m_clientMap.get(clientId);
 	}
 
+	@Nullable
 	public Client registerAuthorizedClient(CentralSocketHandler socketHandler) {
 		Client client;
 		synchronized(this) {
 			client = m_clientMap.computeIfAbsent(socketHandler.getMyID(), a -> new Client(this, m_systemContext, a));
-			client.newConnection(socketHandler);
+			if(!client.newConnection(socketHandler)) {
+				return null;
+			}
 			m_orderedActionList.add(() -> sendClientRegisteredEvent(socketHandler.getMyID()));
 		}
 		runEvents();
